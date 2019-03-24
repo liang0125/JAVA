43a9db753513bdb11474d83d9b3f775904c45804
liang0125 <952787172@qq.com>
2019-03-24 19:42:06 +0800

    Map练习

--- collection/src/main/java/collection/TestArrayList.java
+++ collection/src/main/java/collection/TestArrayList.java
@@ -32,11 +32,77 @@ public class TestArrayList {
 
 
         //Collection接口
-        Collection<String > list=new ArrayList<String>();
-        list.add("hello");
-        list.add("hello");
-        list.add("Java");
-        Object[] objects=list.toArray();
-        System.out.println(Arrays.toString(objects));
+//        Collection<String > list=new ArrayList<String>();
+//        list.add("hello");
+//        list.add("hello");
+//        list.add("Java");
+//        // 操作以Object为主，有可能需要向下转型，就有可能产生ClassCastException
+//        Object[] objects=list.toArray();
+//        System.out.println(Arrays.toString(objects));
+
+
+//        List<String > list=new Vector<String>();
+//        list.add("hello");
+//        list.add("hello");
+//        list.add("Java");
+////        System.out.println(list);
+//        Iterator<String> iterator=list.iterator();
+//        while (iterator.hasNext()){
+//            System.out.println(iterator.next());
+//        }
+
+
+//        Set<String> set=new HashSet<String>();
+//        set.add("hello");
+//        set.add("hello");
+//        set.add("Java");
+//        System.out.println(set);
+
+
+//        Set<Integer> set=new TreeSet<Integer>();
+//        set.add(1);
+//        set.add(1);
+//        set.add(2);
+//        set.add(3);
+//        set.add(4);
+//        set.add(5);
+//        System.out.println(set);
+
+
+//        Map<Integer,String> map=new HashMap<Integer, String>();
+//        map.put(1,"hello");
+//        map.put(1,"hello");
+//        map.put(2,"Java");
+//        map.put(3,"is");
+//        map.put(4,"best");
+//        map.put(null,"!!!");
+//        map.put(null,"...");
+//        map.put(5,null);
+//        map.put(6,null);
+//        System.out.println(map);
+//        System.out.println(map.get(1));
+//        System.out.println(map.get(10));
+
+
+//        Map<Integer,String > map=new Hashtable<Integer, String>();
+//        map.put(1,"hello");
+//        map.put(1,"hello");
+//        map.put(2,"Java");
+//        map.put(3,"is");
+//        map.put(4,"best");
+//        map.put(null,"!!!");
+//        map.put(null,"...");
+//        map.put(5,null);
+//        map.put(6,null);
+//        System.out.println(map.get(1));
+//        System.out.println(map.get(10));
+
+
+        Map<Integer,String> map=new TreeMap<Integer, String>();
+        map.put(2,"is");
+        map.put(1,"Java");
+        map.put(3,"best");
+        map.put(0,"hello");
+        System.out.println(map);
     }
 }
