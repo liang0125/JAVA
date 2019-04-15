98fd82fb15ad0ae649767321fd1cec5d2a939bc0
liang0125 <952787172@qq.com>
2019-04-15 18:09:42 +0800

    每日刷题

---
+++ exercise_2019415/src/main/java/TestOneNumber.java
@@ -0,0 +1,30 @@
+/**
+ * Author:lorrie
+ * Create:2019/4/15
+ *
+ *
+ * 题目描述
+ * 求出1~13的整数中1出现的次数,并算出100~1300的整数中1出现的次数？为此他特别数了一下1~13中包含1的数字有1、10、11、12、13因此共出现6次,但是对于后面问题他就没辙了。
+ * ACMer希望你们帮帮他,并把问题更加普遍化,可以很快的求出任意非负整数区间中1出现的次数（从1 到 n 中1出现的次数）。
+ */
+public class TestOneNumber {
+    public static int NumberOf1Between1AndN_Solution(int n) {
+        if (n<0){
+            return 0;
+        }
+        int count=0;
+        for (int i=0;i<=n;i++){
+            String  str=i+"";
+            char[] c=str.toCharArray();
+            for (int j=0;j<c.length;j++){
+                 if (c[j]=='1'){
+                    count++;
+                 }
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(NumberOf1Between1AndN_Solution(13));
+    }
+}
